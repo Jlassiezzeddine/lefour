13f2fb3c2521dfe6a8d59f78f9dc49d46c1a7523
Jlassi Ezzeddine <jlassiezzeddine@gmail.com>
2023-05-30 22:25:11 +0100

    test with url 127.0.0.1 in server.ts

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -7,5 +7,5 @@ export default ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  url: `http://${env('STRAPI_HOST', '0.0.0.0')}/strapi/`,
+  url: `http://127.0.0.1/strapi/`,
 });
