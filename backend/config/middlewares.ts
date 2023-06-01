f25a99e68a1f0b8fc36457593305e14ccef7b460
Jlassi Ezzeddine <jlassiezzeddine@gmail.com>
2023-06-01 09:57:21 +0100

    Cors config middlewares strapi

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -12,7 +12,14 @@ export default [
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://127.0.0.1:1337', 'http://127.0.0.1:3000']
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
