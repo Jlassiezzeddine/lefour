f93ab0e919a8445b96cdcd408081c2d0bab0a8e2
Jlassi Ezzeddine <jlassiezzeddine@gmail.com>
2023-06-01 18:29:52 +0100

    config docker-compose.yml

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -15,8 +15,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
-      headers: '*',
+      headers: ['*'],
       origin: ['http://127.0.0.1:1337', 'http://127.0.0.1:3005', 'http://127.0.0.1', 'http://51.222.137.40/', 'http://0.0.0.0:3005', 'http://0.0.0.0:1337', 'http://0.0.0.0'],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"]
     }
