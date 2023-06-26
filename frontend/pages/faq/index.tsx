10b749d576630e5581e951781e8fa2a1bbe8020f
Jlassi Ezzeddine <jlassiezzeddine@gmail.com>
2023-06-26 09:41:18 +0100

    fix: faq page fixed

--- frontend/pages/faq/index.tsx
+++ frontend/pages/faq/index.tsx
@@ -5,17 +5,49 @@ import FaqHeader from '@components/faq/FaqHeader';
 import Spacer from '@components/shared/Atoms/Spacer';
 import SeoHeader from '@components/shared/SeoHeader';
 import Container from '@mui/material/Container';
+import Fuse from 'fuse.js';
 import type { NextPage } from 'next';
 import { useState } from 'react';
-import useFaqFilter from 'src/common/hooks/useFaqFilter';
-import { FAQContext } from 'src/common/types/FAQ';
+import { useFrequentlyAskedQuestions } from 'src/common/hooks/api/faq';
+import { FAQContext, IFaq } from 'src/common/types/FAQ';
 
 const FAQ: NextPage = () => {
   const [selectedCategory, setSelectedCategory] = useState<FAQContext | null>(
     null
   );
   const [searchQuery, setSearchQuery] = useState<string>('');
-  const { faqItems } = useFaqFilter({ searchQuery, selectedCategory });
+  const faqList = useFrequentlyAskedQuestions() || [];
+  const fuse = new Fuse(faqList, {
+    // isCaseSensitive: false,
+    includeScore: true,
+    // shouldSort: true,
+    includeMatches: false,
+    // findAllMatches: true,
+    //   minMatchCharLength: 1,
+    // location: 0,
+    threshold: 0.4,
+    distance: 300,
+    useExtendedSearch: true,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    // fieldNormWeight: 1,
+    keys: ['question'],
+  });
+
+  const getFaqItems = (faqList: IFaq[]) => {
+    let result = faqList || [];
+    if (searchQuery) {
+      result = fuse
+        .search({
+          $or: searchQuery.split(',').map((el) => ({ question: el.trim() })),
+        })
+        .map((e) => e.item);
+    }
+    if(selectedCategory){
+      result = result.filter(e => e.context === selectedCategory)
+    }
+    return result
+  };
 
   return (
     <>
@@ -30,7 +62,9 @@ const FAQ: NextPage = () => {
             setSelectedCategory={setSelectedCategory}
           />
           <Spacer size={8} />
-          <FaqAccordion faqItems={faqItems} />
+          <FaqAccordion
+            faqItems={getFaqItems(faqList)}
+          />
           <Spacer size={16} />
           <FaqContact />
           <Spacer size={8} />
